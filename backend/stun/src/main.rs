dcbb8989c4574dee9ab3b65b9bd15afc259a2154
8-prime <hoffmeister.stefan99@gmail.com>
2026-09-08 00:59:48 +0200

    some more getting use to rfc 8489

--- backend/stun/src/main.rs
+++ backend/stun/src/main.rs
@@ -2,6 +2,61 @@ use socket2::{Domain, Protocol, Socket, Type};
 use std::{io, net::SocketAddr};
 use tokio::{net::UdpSocket, task::JoinSet};
 
+enum StunClass {
+    Request,
+    Indication,
+    Success,
+    ErrorResponse,
+}
+
+impl TryFrom<u16> for StunClass {
+    type Error = ();
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        match value {
+            0b00 => Ok(StunClass::Request),
+            0b01 => Ok(StunClass::Indication),
+            0b10 => Ok(StunClass::Success),
+            0b11 => Ok(StunClass::ErrorResponse),
+            _ => Err(()),
+        }
+    }
+}
+
+const BINDING: u8 = 0b1;
+
+enum MessageType {}
+
+struct StunMessageType {
+    class: StunClass,
+    is_binding: bool,
+}
+
+impl TryFrom<u16> for StunMessageType {
+    type Error = ();
+
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        if ((value & 0b1100_0000_0000_0000) >> 14) != 0b00 {
+            return Err(());
+        }
+
+        let message_low = value & 0b0000_0000_0000_1111;
+        let message_mid = (value & 0b0000_0000_1110_0000) >> 1;
+        let message_high = (value & 0b0011_1110_1110_0000) >> 2;
+        let message = message_high | message_mid | message_low;
+
+        let class_low = (value & 0b0000_0000_0001_0000) >> 4;
+        let class_high = (value & 0b0000_0001_0000_0000) >> 7;
+        let class = class_high | class_low;
+        let stun_class = class.try_into()?;
+
+        return Ok(StunMessageType {
+            class: StunClass::Indication,
+            is_binding: true,
+        });
+    }
+}
+
 fn bind_worker(addr: SocketAddr) -> io::Result<UdpSocket> {
     let socket = Socket::new(Domain::for_address(addr), Type::DGRAM, Some(Protocol::UDP))?;
 
